56134bfd65f9f463826dcd2601938b44a2a8b031
Tomajo <tmassojou@gmail.com>
2014-11-23 10:41:54 +0100

    Afegits comentaris per entendre el codi en el futur

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Serveix per calcular la inversa d'una matriu només si no s'ha calculat abans.
+##fa us del <<- que serveix per fer l'assignació amb un altre scope.
+##Primer has de cridar la funció makeCache matrix passant una matriu
+##DEsprés pots demanar la inversa (CacheSolve) tantes vegades com vulguis, només et calcularà la primera vegada o si 
+##ha canviat
 
-## Write a short comment describing this function
+## Es un objecte que retorna una llista de funcions on hi ha gets i sets tant de la matriu en si com de la inversa
+##Se li passa una matriu, retorna una llista de funcions
 
 makeCacheMatrix <- function(x = matrix()) {
   
@@ -20,7 +24,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-## Write a short comment describing this function
+##Mira si la matriu ja està calculada.
+## Calcula la inversa de la matriu si no ho està
+##Se li passa una llista creada a makeCacheMatrix, et retorna una matriu inversa.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
